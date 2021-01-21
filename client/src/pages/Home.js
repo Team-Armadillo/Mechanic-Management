b69d24be5d1cf2998b994d8f4234106aeb619ab0
Ender <woodwind.turbeville@gmail.com>
2021-01-21 12:30:32 -0600

    Added new code for initializing the Home Page and installed required dependencies

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <p>Welcome to Mechanic-Management!</p>
+    </div>
+  );
+};
+
+export default Home;
