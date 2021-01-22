d08660fd93bace6c48662d8a4a7f6ed1f76c039c
BMcCool <b.mccoolpierce@gmail.com>
2021-01-21 19:53:59 -0600

    removed font awesome to fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 
+
+
 const Home = () => {
   return (
     <div className="container">
+      
       <p>Welcome to Mechanic-Management!</p>
     </div>
   );
