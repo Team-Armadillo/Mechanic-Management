a6ed54e2be647696d835949bd16a93899f12f4e5
BMcCool <b.mccoolpierce@gmail.com>
2021-01-21 20:49:43 -0600

    simple navbar added with basic css

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,6 +5,10 @@ import { ReactComponent as Logo } from "../assets/logo.svg";
 import "./header.css";
 
 const Header = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  
   return (
     <div className="header">
       <div className="logo-nav">
@@ -13,17 +17,21 @@ const Header = () => {
             <Logo className="logo" />
           </a>
         </div>
-        <ul>
-          <li>
+
+        <ul className={click ? "nav-options active" : "nav-options"}>
+          <li className="option" onClick={closeMobileMenu}>
             <a href="#">ABOUT</a>
           </li>
-          <li>
+          <li className="option" onClick={closeMobileMenu}>
             <a href="#">CONTACT</a>
           </li>
-          <li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">BLOG</a>
+          </li>
+          <li className="option mobile-option" onClick={closeMobileMenu}>
             <a href="#">SIGN-IN</a>
           </li>
-          <li>
+          <li className=" option mobile-option" onClick={closeMobileMenu}>
             <a href="" className="sign-up">
               SIGN-UP
             </a>
@@ -31,15 +39,22 @@ const Header = () => {
         </ul>
       </div>
       <ul className="signin-up">
-        <li>
+        <li className="sign-in" onClick={closeMobileMenu}>
           <a href="#">SIGN-IN</a>
         </li>
-        <li>
+        <li onClick={closeMobileMenu}>
           <a href="" className="signup-btn">
             SIGN-UP
           </a>
         </li>
       </ul>
+      <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <CloseMenu className="menu-icon" />
+        ) : (
+          <MenuIcon className="menu-icon" />
+        )}
+      </div>
     </div>
   );
 };
