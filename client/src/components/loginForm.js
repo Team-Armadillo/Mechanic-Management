ad01c350b1e04649c8e4e5390009928cafd689b6
Octavio Garcia <octaviogarciapc@gmail.com>
2021-01-23 21:06:08 -0600

    login css

--- client/src/components/loginForm.js
+++ client/src/components/loginForm.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import LoginBtn from './LoginBtn';
-
-
+import './LoginForm.css';
 
 function LoginForm() {
     return (
        <form>
            <div className= "form-inner">
            <h1>Mecanico</h1>
-           <h3>Dashboard Login</h3>
+           <p>Dashboard Login</p>
            {/*error*/}
 
            <div className="form-group">
